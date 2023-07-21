3cd6fe171f31e43eae7e1f91c0bf6f4b6c05003f
VincentYeh-dev <vincent3314534>
2023-07-21 18:56:12 +0800

    修復視窗關閉，無法關閉程式問題

--- src/main/java/org/vincentyeh/img2pdf/gui/App.java
+++ src/main/java/org/vincentyeh/img2pdf/gui/App.java
@@ -4,17 +4,27 @@ import com.formdev.flatlaf.FlatDarkLaf;
 import org.vincentyeh.img2pdf.gui.ui.MainFrame;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 //        FlatDarkLaf.setup();
-        JFrame frame= new JFrame(Constants.APP_TITLE);
+        JFrame frame = new JFrame(Constants.APP_TITLE);
+//        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                frame.dispose();
+                System.exit(0);
+            }
+        });
+
         frame.setContentPane(new MainFrame().getRootPanel());
         frame.pack();
         frame.setVisible(true);
