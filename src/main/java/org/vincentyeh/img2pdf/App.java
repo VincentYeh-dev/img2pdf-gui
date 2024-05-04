094d53d55f32ddb2412b6eb54aafc68c0ffa7a54
vincent <vincent@DESKTOP-FPPVIOM>
2024-05-05 02:00:50 +0800

    init commit

---
+++ src/main/java/org/vincentyeh/img2pdf/App.java
@@ -0,0 +1,40 @@
+package org.vincentyeh.img2pdf;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import org.vincentyeh.img2pdf.controller.Controller;
+import org.vincentyeh.img2pdf.model.Model;
+import org.vincentyeh.img2pdf.view.View;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+
+    public static void main(String[] args) {
+        FlatDarkLaf.setup();
+
+        JFrame frame = new JFrame(Constants.APP_TITLE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                frame.dispose();
+                System.exit(0);
+            }
+        });
+
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+        controller.initialize();
+
+        frame.setContentPane(view.getRootPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/org/vincentyeh/img2pdf/Constants.java
@@ -0,0 +1,8 @@
+package org.vincentyeh.img2pdf;
+
+public class Constants {
+    private Constants(){
+
+    }
+    public static final String APP_TITLE="img2pdf-gui";
+}
